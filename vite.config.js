a167635d0888af1b0f1d5b8c1914eeb753046073
APT <paulustindi@gmail.com>
2022-10-09 19:10:05 +0800

    Theme

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,19 @@ import laravel, { refreshPaths } from 'laravel-vite-plugin'
 export default defineConfig({
     plugins: [
         laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
+            refresh: true,
+        }),
+    ],
+});
+/*export default defineConfig({
+    plugins: [
+        laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: [...refreshPaths, 'app/Http/Livewire/**'],
         }),
     ],
-})
+})*/
