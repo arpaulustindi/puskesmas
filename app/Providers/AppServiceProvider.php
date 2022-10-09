95530746a8e43136bc97011ba9833a46d7cdc4bc
APT <paulustindi@gmail.com>
2022-10-09 22:09:37 +0800

    FIx Theme Build Error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Yepsua\Filament\Themes\Facades\FilamentThemes;
 class AppServiceProvider extends ServiceProvider
 {
     /**
